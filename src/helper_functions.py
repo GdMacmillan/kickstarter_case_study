433270e511e86f5f4afd1a0619c7ff322889148e
Gordon MacMillan <gmacilla@ymail.com>
2017-10-09 15:27:40 -0600

    modeling started + a little more data exploration

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -3,8 +3,46 @@ import glob
 import os
 
 # third party imports
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import scipy.stats as sp
+
+from nltk.corpus import stopwords
+from sklearn.base import TransformerMixin
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+from sklearn.metrics import confusion_matrix
+
+# A custom stoplist
+STOPLIST = set(stopwords.words('english') + ["n't", "'s", "'m", "ca"] + list(ENGLISH_STOP_WORDS))
+
+# Define a custom transformer to clean text using spaCy
+class CleanTextTransformer(TransformerMixin):
+    """
+    Convert text to cleaned text
+    """
+
+    def transform(self, X, **transform_params):
+        return X.astype(str).apply(lambda x: clean_text(x))
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def get_params(self, deep=True):
+        return {}
+
+def clean_text(text):
+    # get rid of newlines
+    text = text.strip().replace("\n", " ").replace("\r", " ").replace("\t", " ")
+
+    # replace HTML symbols
+    text = text.replace("&amp;", "and").replace("&gt;", ">").replace("&lt;", "<")
+
+    # lowercase
+    text = text.lower()
+
+    split_text = text.split()
+    return " ".join([word for word in split_text if word not in STOPLIST])
 
 def load_dataset():
 
@@ -28,6 +66,74 @@ def kstest_by(vals, by):
     vals2 = vals[-by]
 
     return sp.stats.ks_2samp(vals1, vals2)
+
+def roc_curve(probabilities, labels):
+    '''
+    INPUT: numpy array, numpy array
+    OUTPUT: list, list, list
+
+    Take a numpy array of the predicted probabilities and a numpy array of the
+    true labels.
+    Return the True Positive Rates, False Positive Rates and Thresholds for the
+    ROC curve.
+    '''
+
+    thresholds = np.sort(probabilities)
+
+    tprs = []
+    fprs = []
+
+    num_positive_cases = sum(labels)
+    num_negative_cases = len(labels) - num_positive_cases
+
+    for threshold in thresholds:
+        # With this threshold, give the prediction of each instance
+        predicted_positive = probabilities >= threshold
+        # Calculate the number of correctly predicted positive cases
+        true_positives = np.sum(predicted_positive * labels)
+        # Calculate the number of incorrectly predicted positive cases
+        false_positives = np.sum(predicted_positive) - true_positives
+        # Calculate the True Positive Rate
+        tpr = true_positives / float(num_positive_cases)
+        # Calculate the False Positive Rate
+        fpr = false_positives / float(num_negative_cases)
+
+        fprs.append(fpr)
+        tprs.append(tpr)
+
+    return tprs, fprs, thresholds.tolist()
+
+def plot_roc(probs, y_true, title, xlabel, ylabel):
+    # ROC
+    tpr, fpr, thresholds = roc_curve(probs, y_true)
+
+    # plt.hold(True)
+    plt.plot(fpr, tpr)
+
+    # 45 degree line
+    xx = np.linspace(0, 1.0, 20)
+    plt.plot(xx, xx, color='red')
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+
+    plt.show()
+
+
+def plot_confusion_matrix(model, X_test, y_true):
+    '''Code from sklearn example'''
+    cm = confusion_matrix(y_true, model.predict(X_test))
+
+    print(cm)
+
+    # Show confusion matrix in a separate window
+    plt.matshow(cm)
+    plt.title('Confusion matrix')
+    plt.colorbar()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
 
 def CustomParser(data):
     import json
